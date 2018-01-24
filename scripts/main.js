2dee227b73fff0709f3f9784b93294a3d6302604
Brad Swan <bradj.swan@gmail.com>
2018-01-25 01:02:47 +1000

    added username entry

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,21 @@ myImage.onclick = function() {
 		myImage.setAttribute ('src', 'images/firefox-logo.jpg');
 	}
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent =  'Mozilla is cool, ' + myName;
+}
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
