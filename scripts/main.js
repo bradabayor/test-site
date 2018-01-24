ac81919296d7038a86583fd1f9765444a63bd33b
Brad Swan <bradj.swan@gmail.com>
2018-01-25 00:54:25 +1000

    added photo swap

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World';
+
+var myImage = document.querySelector('img');
+
+myImage.onClick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/firefox-logo.jpg') {
+		myImage.setAttribute ('src', 'images/chrome-logo.png');
+	} else {
+		myImage.setAttribute ('src', 'images/firefox-logo.jpg');
+	}
+}
